f4eba210762b96b59b8e447462caf85548385e1a
Kaj Norman <knn@eaaa,dk>
2020-05-02 11:52:43 +0200

    data fetched from mqtt and saved in one table

--- mqttdatabaseserver.py
+++ mqttdatabaseserver.py
@@ -1,4 +1,15 @@
 import paho.mqtt.client as mqtt
+import sqlite3
+dbFile =  "data/data.db"
+
+
+
+def writeToDb(Key, Value):
+    conn = sqlite3.connect(dbFile)
+    c = conn.cursor()
+    print( "Writing to db...")
+    c.execute("INSERT INTO full_table (reading_time ,key ,value) VALUES (DATETIME('now'),?,?)", (Key, Value))
+    conn.commit()
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -8,6 +19,9 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+    writeToDb(msg.topic , str(msg.payload))
+
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
